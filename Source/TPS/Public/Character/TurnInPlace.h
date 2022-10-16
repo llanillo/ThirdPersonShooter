8cb892d0988da4d42a0ccd6e784603c940010c54
Octavio Llanillo <llanillo4@hotmail.com>
2022-10-16 01:01:42 -0300

    Create turn in place enum

---
+++ Source/TPS/Public/Character/TurnInPlace.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "TurnInPlace.generated.h"
+
+UENUM(BlueprintType)
+enum class ETurnInPlace : uint8
+{
+	ETIP_Left UMETA(DisplayName = "Turning Left"),
+	ETIP_Right UMETA(DisplayName = "Turning Right"),
+	ETIP_NotTurning UMETA(DisplayName = "Not Turning"),
+	ETIP_MAX UMETA(DisplayName = "DefaultMAX")
+};
