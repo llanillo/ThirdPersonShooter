0a5e5fc9d237fe7ac70e57695fe5ef129ae1d3a6
Octavio Llanillo <llanillo4@hotmail.com>
2022-10-23 23:27:04 -0300

    Add bullet shells physics

--- Source/TPS/Private/Weapon/BulletShell.cpp
+++ Source/TPS/Private/Weapon/BulletShell.cpp
@@ -1,17 +1,44 @@
 ﻿#include "Weapon/BulletShell.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/GameplayStatics.h"
 
 ABulletShell::ABulletShell()
 {
 	ShellMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Component"));
 	ShellMeshComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
 	ShellMeshComponent->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
+	ShellMeshComponent->SetSimulatePhysics(true);
+	ShellMeshComponent->SetEnableGravity(true);
+	ShellMeshComponent->SetNotifyRigidBodyCollision(true);
 	SetRootComponent(ShellMeshComponent);
+	
 }
 
 void ABulletShell::BeginPlay()
 {
 	Super::BeginPlay();
+
+	AActor::SetLifeSpan(BulletLifeSpan);
 	
+	ShellMeshComponent->AddImpulse(GetActorForwardVector() * EjectionImpulse);
+
+	if (HasAuthority())
+	{
+		ShellMeshComponent->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
+	}
 }
 
-
+void ABulletShell::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                         FVector NormalImpulse, const FHitResult& Hit)
+{
+	if(bAlreadyPlayedSound) return;
+	
+	if (const UWorld* World = GetWorld())
+	{
+		if (DropSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(World, DropSound, GetActorLocation());
+			bAlreadyPlayedSound = true;
+		}
+	}
+}
--- Source/TPS/Public/Weapon/BulletShell.h
+++ Source/TPS/Public/Weapon/BulletShell.h
@@ -4,6 +4,8 @@
 #include "GameFramework/Actor.h"
 #include "BulletShell.generated.h"
 
+class USoundCue;
+
 UCLASS(ClassGroup=TPS)
 class TPS_API ABulletShell : public AActor
 {
@@ -11,14 +13,35 @@ class TPS_API ABulletShell : public AActor
 
 	UPROPERTY(EditAnywhere, Category = Components)
 	UStaticMeshComponent* ShellMeshComponent;
+
+	UPROPERTY(EditAnywhere, Category = Properties)
+	USoundCue* DropSound;
+
+	UPROPERTY()
+	FTimerHandle DestroyHandle;
+	
+	UPROPERTY(EditAnywhere, Category = Properties)
+	float EjectionImpulse = 5.f;
+
+	UPROPERTY(EditAnywhere, Category = Properties)
+	float BulletLifeSpan = 15.f;
+
+	
+protected:
+
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	                   FVector NormalImpulse, const FHitResult& Hit);
+
+private:
+
+	bool bAlreadyPlayedSound = false;
 	
 public:
 	
 	ABulletShell();
 
 protected:
-
+	
 	virtual void BeginPlay() override;
-
-public:
 };
